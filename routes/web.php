92eb4f1447b5051cd0628c09489029d0ca63adcf
darektoa <darektoa@gmail.com>
2021-10-12 23:13:24 +0700

    Added: Login token migration and model

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('welcome'));
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
