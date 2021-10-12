098ac9152f957c94e3baa6553a665a5f37ca5723
darektoa <darektoa@gmail.com>
2021-10-12 22:54:19 +0700

    Added: OTP Number table in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('OTP Numbers') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,29 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Number</th>
+                                <th scope="col">OTP From</th>
+                                <th scope="col">OTP Number</th>
+                                <th scope="col">Datetime</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach(App\Models\OtpNumber::all() as $item)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td>{{ $item->number }}</td>
+                                <td>{{ $item->otp_from }}</td>
+                                <td>{{ $item->otp_number }}</td>
+                                <td>{{ $item->datetime }}</td>
+                            </tr>
+                            @endforeach
+
+                      </table>
                 </div>
             </div>
         </div>
